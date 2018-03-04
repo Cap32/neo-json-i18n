e93b623449c6b1c68649bb1917a8a9f56f54af16
Webb Peng <webb.png@gmail.com>
2018-03-04 23:08:08 +0800

    update

--- src/translateJsonValues.js
+++ src/translateJsonValues.js
@@ -1,22 +1,70 @@
 import translate from 'google-translate-api';
 import { resolve, dirname, extname, basename } from 'path';
+import { ensureDir, readJson, outputJson } from 'fs-extra';
 
-export default function translateJsonValues(options = {}) {
-	const { cwd, src, output, pattern, lang: langs } = options;
-	const source = resolve(cwd, src);
-	const dist = resolve(cwd, output || dirname(source));
-	const extWithDot = extname(source);
-	const ext = extWithDot.slice(1);
-	const originalName = basename(source, extWithDot);
+export default async function translateJsonValues(options = {}) {
+	try {
+		const { cwd, src, output, pattern, lang: langs } = options;
+		const source = resolve(cwd, src);
+		const dist = resolve(cwd, output || dirname(source));
+		const extWithDot = extname(source);
+		const ext = extWithDot.slice(1);
+		const originalName = basename(source, extWithDot);
 
-	langs.map((lang) => {
-		const outputName = pattern
-			.replace('$name', originalName)
-			.replace('$lang', lang)
-			.replace('$ext', ext);
-		const outputFile = resolve(dist, outputName);
-		console.log('outputFile', outputFile);
-	});
+		const sourceJson = await readJson(source);
 
-	console.log('options', source, dist, langs);
+		const execTranslate = async function execTranslate(json, lang) {
+			const traverse = async function traverse(json) {
+				if (Array.isArray(json)) {
+					return Promise.all(json.map(traverse));
+				}
+				else if (typeof json === 'object') {
+					const acc = {};
+					await Promise.all(
+						Object.keys(json).map(async (key) => {
+							acc[key] = await traverse(json[key]);
+						}),
+					);
+					return acc;
+				}
+				else if (typeof json === 'string') {
+					console.log('json', json);
+					return 'hello';
+
+					const { text } = translate(json, { to: lang });
+					return text;
+				}
+				else {
+					return json;
+				}
+			};
+			return traverse(json);
+		};
+
+		await ensureDir(dist);
+
+		const res = {};
+
+		await Promise.all(
+			langs.map(async (lang) => {
+				const outputName = pattern
+					.replace('$name', originalName)
+					.replace('$lang', lang)
+					.replace('$ext', ext);
+				const outputFile = resolve(dist, outputName);
+				const resData = await execTranslate(sourceJson, lang);
+				await outputJson(outputFile, resData, { spaces: '\t' });
+				res[lang] = resData;
+			}),
+		);
+
+		console.log('done');
+		return res;
+	}
+	catch (err) {
+		console.log('fork?');
+		console.error(err);
+
+		// process.exit(1);
+	}
 }
--- test/test.js
+++ test/test.js
@@ -1,9 +1,25 @@
-import { execSync } from 'child_process';
-import { resolve } from 'path';
-import { bin } from '../package.json';
+import translateJsonValues from '../src/translateJsonValues';
 
-test('🌚 ', () => {
-	const binFile = Object.keys(bin)[0];
-	const command = resolve(`bin/${binFile}`);
-	console.log(execSync(`${command}`, { encoding: 'utf8' }));
+describe('translate json values', function () {
+	it('test', async () => {
+
+		// console.log('done');
+
+		console.log(
+			'typeof translateJsonValues',
+			await translateJsonValues({
+				src: 'fixtures/index.json',
+				cwd: __dirname,
+				lang: ['en'],
+				pattern: '$name_$lang.$ext',
+			}),
+		);
+
+		// const res = await translateJsonValues({ src: 'fixtures/index.json' });
+		// console.log('res', res);
+
+		// const binFile = Object.keys(bin)[0];
+		// const command = resolve(`bin/${binFile}`);
+		// console.log(execSync(`${command}`, { encoding: 'utf8' }));
+	});
 });
