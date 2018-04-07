a7912cb3308230022b5c402f8b07cf3c15775023
Webb Peng <webb.png@gmail.com>
2018-04-07 10:01:56 +0800

    fix testing on windows

--- test/cli.test.js
+++ test/cli.test.js
@@ -1,4 +1,4 @@
-import { execSync } from 'child_process';
+import spawn from 'cross-spawn';
 import { resolve } from 'path';
 import { readJson, remove } from 'fs-extra';
 
@@ -13,7 +13,7 @@ describe('cli', function () {
 		const src = 'src/a.json';
 		const bin = resolve('bin/neo-json-i18n');
 		const cwd = 'test/fixtures';
-		execSync(`${bin} ${src} -l=zh-cn -o=dist -d=${cwd}`);
+		spawn.sync(bin, `${src} -l=zh-cn -o=dist -d=${cwd}`.split(' '));
 		const dist = resolve('test/fixtures/dist/a_zh-cn.json');
 		expect(await readJson(dist)).toEqual({
 			film: '星球大战',
@@ -23,7 +23,7 @@ describe('cli', function () {
 	test('should `--config` work', async () => {
 		const src = 'src/a.json';
 		const bin = resolve('bin/neo-json-i18n');
-		execSync(`${bin} ${src} -c=test/fixtures/src/i18nconfig.js`);
+		spawn.sync(bin, `${src} -c=test/fixtures/src/i18nconfig.js`.split(' '));
 		const dist = resolve('test/fixtures/dist/zh_CN.json');
 		expect(await readJson(dist)).toEqual({
 			lang: 'zh_CN',
